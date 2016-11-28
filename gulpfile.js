58cfba9a1d57674d91367e1e61e4da973c3b3a7e
Maksym Ridush <maks@cog1.com>
2016-11-28 18:12:29 +0200

    make deploys manually

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,5 @@
 var gulp = require('gulp');
-var debug = require('gulp-debug');
+
 gulp.task('build', function () {
-    var stream = gulp.src(__dirname + '/client/dist/static/**/*');
-    stream.pipe(debug());
-    return stream.pipe(gulp.dest(__dirname + "/public"));
-});
-
-gulp.task('css', function () {
-    return gulp.src(__dirname + '/client/src/client/public/style.css').pipe(gulp.dest(__dirname + "/public/css"));
-});
-
-gulp.task('js', function () {
-    return gulp.src(__dirname + '/client/src/client/public/app.bundle.js').pipe(gulp.dest(__dirname + "/public/js"));
+    return  gulp.src(__dirname + '/client/dist/static/**/*').stream.pipe(gulp.dest(__dirname + "/public"));
 });

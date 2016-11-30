c0e21e79867c63d49a15a6dcc179c0908cf4e3f5
Maksym Ridush <maks@cog1.com>
2016-11-30 15:28:40 +0200

    update client constants

--- client/src/constants/common.js
+++ client/src/constants/common.js
@@ -1,9 +1,2 @@
-export const BASE_URL = 'http://dev.overgarden.com';
+export const BASE_URL = 'http://localhost:9000/';
 export const AUTH_COOKIE_KEY = 'authContext';
-export const JWT_PAYLOAD_ROLES_KEY = 'http://schemas.microsoft.com/ws/2008/06/identity/claims/role';
-//export const BASE_URL = 'http://localhost:35421';
-//export const BASE_URL = 'http://localhost:54337';
-
-
-export const TABLE_ROW_COUNT = 10;
-

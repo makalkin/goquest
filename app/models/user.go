39ae16c0bfaf8c3a1d5e4ccb9d74b3ffad597e82
Maksym Ridush <maks@cog1.com>
2016-11-30 15:48:01 +0200

    add UserCircle structure

--- app/models/user.go
+++ app/models/user.go
@@ -4,14 +4,16 @@ import (
 	"gopkg.in/mgo.v2"
 )
 
+type UserCircle struct {
+	Circle     mgo.DBRef `bson:",omitempty" json:"circle"`
+	Experience int       `json:"experience"`
+	Currency   int       `json:"currency"`
+}
+
 type User struct {
 	Base        `bson:",inline"`
 	AccessToken string `bson:"access_token" json:"accessToken,omitempty"`
 	Name        string `json:"name"`
 	Fid         string `json:"fid,omitempty"`
-	Circles     []struct {
-		Circle     mgo.DBRef `bson:",omitempty" json:"circle"`
-		Experience int       `json:"experience"`
-		Currency   int       `json:"currency"`
-	} `bson:",omitempty" json:"circles,omitempty"`
+	Circles     []UserCircle `bson:",omitempty" json:"circles,omitempty"`
 }

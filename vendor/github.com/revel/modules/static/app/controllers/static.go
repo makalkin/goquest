9d89a2f1857c510730b25402a60ef045d057f0a1
Maks Ridush <makalkon@bigmir.net>
2017-01-07 16:13:44 +0200

    update dependencies

--- vendor/github.com/revel/modules/static/app/controllers/static.go
+++ vendor/github.com/revel/modules/static/app/controllers/static.go
@@ -74,6 +74,7 @@ func (c Static) ServeModule(moduleName, prefix, filepath string) revel.Result {
 	return serve(c, absPath, filepath)
 }
 
+
 // This method allows static serving of application files in a verified manner.
 func serve(c Static, prefix, filepath string) revel.Result {
 	var basePath string
